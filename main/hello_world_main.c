4e27a463265a134385e6838b5a3527703e100e4b
Mike Kostenko <vopros.m@gmail.com>
2025-04-20 12:36:17 +0100

    refactor: update LED GPIO definitions and configure multiple LEDs for blinking

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -16,14 +16,27 @@
 #include "esp_log.h"
 
 static const char *TAG = "blink_example";
-#define BLINK_GPIO 6  // Change this to match your board's LED pin
-#define BLINK_PERIOD_MS 1000  // Blink period in milliseconds
+#define BLINK_GPIO_white 2
+#define BLINK_GPIO_red 3
+#define BLINK_GPIO_blue 4
+#define BLINK_GPIO_green 5
+#define BLINK_GPIO_yellow 6  // Change this to match your board's LED pin
+#define BLINK_PERIOD_MS 500  // Blink period in milliseconds
+#define SWITCH_PERIOD_MS 100  // Blink period in milliseconds
 
 void app_main(void)
 {
     // Configure the GPIO pin
-    gpio_reset_pin(BLINK_GPIO);
-    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+    gpio_reset_pin(BLINK_GPIO_white);
+    gpio_set_direction(BLINK_GPIO_white, GPIO_MODE_OUTPUT);
+    gpio_reset_pin(BLINK_GPIO_red);
+    gpio_set_direction(BLINK_GPIO_red, GPIO_MODE_OUTPUT);
+    gpio_reset_pin(BLINK_GPIO_blue);
+    gpio_set_direction(BLINK_GPIO_blue, GPIO_MODE_OUTPUT);
+    gpio_reset_pin(BLINK_GPIO_green);
+    gpio_set_direction(BLINK_GPIO_green, GPIO_MODE_OUTPUT);
+    gpio_reset_pin(BLINK_GPIO_yellow);
+    gpio_set_direction(BLINK_GPIO_yellow, GPIO_MODE_OUTPUT);
     
     ESP_LOGI(TAG, "Blink example started");
     
@@ -34,7 +47,20 @@ void app_main(void)
         led_state = !led_state;
         
         // Set the GPIO level according to the state
-        gpio_set_level(BLINK_GPIO, led_state);
+        gpio_set_level(BLINK_GPIO_white, led_state);
+        vTaskDelay(BLINK_PERIOD_MS / portTICK_PERIOD_MS);
+        
+        gpio_set_level(BLINK_GPIO_red, led_state);
+        vTaskDelay(BLINK_PERIOD_MS / portTICK_PERIOD_MS);
+        
+        gpio_set_level(BLINK_GPIO_blue, led_state);
+        vTaskDelay(BLINK_PERIOD_MS / portTICK_PERIOD_MS);
+        
+        gpio_set_level(BLINK_GPIO_green, led_state);
+        vTaskDelay(BLINK_PERIOD_MS / portTICK_PERIOD_MS);
+        
+        gpio_set_level(BLINK_GPIO_yellow, led_state);
+        vTaskDelay(BLINK_PERIOD_MS / portTICK_PERIOD_MS);
         
         ESP_LOGI(TAG, "Turning the LED %s!", led_state ? "ON" : "OFF");
         
